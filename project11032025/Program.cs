2257707381dbe1f483c21e5a33aa25d1c51dbedb
Екатерина Омельницкая <k625972@gmail.com>
2025-11-03 17:04:35 +0500

    Добавьте файлы проекта.

---
+++ project11032025/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace project11032025
+{
+    internal class Program
+    {
+        /*
+         * пользователь в бесконечном цикле осуществляет
+         * построчный ввод. 
+         * цикл останавливается при условие ввода пустой строки
+         * или стоки "exit" или "stop"
+         * после чего пользователю выводится количество символов, которые он задал считать
+         * */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("какой символ будем считать в тексте?");
+            char symbol = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("осуществите ввод текста построчно");
+            int countOfChar = 0;
+
+            while (true)
+            {
+                string temp = Console.ReadLine();
+                if (temp == "" || temp == "stop" || temp == "exit")
+                    break;
+
+                foreach (char ch in temp)
+                {
+                    if (ch == symbol)
+                        countOfChar++;
+                }
+
+            }
+            Console.WriteLine("вы ввели " + countOfChar + " символов " + symbol);
+        }
+    }
+}
